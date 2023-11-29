35f56ba7edb4d79b783cd54a45d7c7b3f26204a6
Yannick Van den Brande <yavadebr@gmail.com>
2023-11-29 17:40:16 +0100

    feat: add pfp + name

--- src/components/Video/VideoDetails.vue
+++ src/components/Video/VideoDetails.vue
@@ -1,10 +1,17 @@
 <script setup>
-    //define props
-   const props = defineProps(['description']);
+//define props
+const props = defineProps(['description']);
 </script>
 
 <template>
-  <h3>sami2bx</h3>
+  <div class="user">
+    <img src="https://i.imgur.com/ExsMDsS.png" alt="user image">
+    <div class="username">
+      <h3>sami2bx</h3>
+      <h4>Sami Something</h4>
+    </div>
+  </div>
+
   <p>{{ description }}</p>
 </template>
 
