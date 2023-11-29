beb09dcb2c6ec03e35cf1bef00555b6af6188309
Yannick Van den Brande <yavadebr@gmail.com>
2023-11-29 16:47:54 +0100

    feat: GET messages from API

--- src/components/Video/Chat/Chat.vue
+++ src/components/Video/Chat/Chat.vue
@@ -1,33 +1,51 @@
 <script setup>
-    // import ref, reactive, onMounted
-    import { ref ,reactive, onMounted} from 'vue';
+// import ref, reactive, onMounted
+import { ref, reactive, onMounted } from 'vue';
 
-    let message = ref(""); // int, string, boolean
-    let allMessages = reactive({
-        data: [],
-    });
+let message = ref(""); // int, string, boolean
+let allMessages = reactive({
+    data: [],
+});
 
-    //function sendMessage
-    const sendMessage = () => {
-        allMessages.data.push(message.value);
-        message.value="";
-    };
+// GET request to the API for messages when component is mounted
+onMounted(async () => {
+    try {
+        const response = await fetch('https://nodejs-messages.onrender.com/api/v1/messages');
+        const data = await response.json();
+
+        if (data.status === 'success') {
+            // update the local messages data with the received messages
+            allMessages.data = data.data[0].messages;
+        } else {
+            console.error('Failed to fetch messages:', data.message);
+        }
+    } catch (error) {
+        console.error('Error fetching messages:', error.message);
+    }
+});
+
+
+// function sendMessage
+const sendMessage = () => {
+    allMessages.data.push(message.value);
+    message.value = "";
+};
 
 </script>
 
 <template>
-  <div>
-    <ul>
-        <li v-for="m in allMessages.data">{{ m }}</li>
-    </ul>
-
     <div>
-        <input v-model="message" type="text" placeholder="">
-        <button @click="sendMessage">Send</button>
+        <ul>
+            <li v-for="m in allMessages.data" :key="m._id">
+                {{ m.user }}: {{ m.message }}
+            </li>
+        </ul>
+
+        <div>
+            <input v-model="message" type="text" placeholder="">
+            <button @click="sendMessage">Send</button>
+        </div>
     </div>
-  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
